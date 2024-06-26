ee85c2c47680f51a286bc56b96f85e07b39ea5f1
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-06-26 00:19:22 -0500

    Manage Tags

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-// use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Authenticatable
 {
-    use /*HasApiTokens,*/ HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AdminController::class, 'login'])->name('admin.login');
 Route::post('auth', [AdminController::class, 'auth'])->name('admin.auth');
+Route::middleware('admin')->get('/admin', function () {
+    return view('dashboard');
+});
+
 
 Route::prefix('admin')->group(function () {
+
+
     Route::get('dashboard', [AdminController::class, 'index'])->name('admin.index');
     Route::post('logout', [AdminController::class, 'logout'])->name('admin.logout');
     //tags routes
