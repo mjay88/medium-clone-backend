064c07b5939eabb3925f3d5dcdb2179fb58bb05a
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-07-19 21:51:08 -0500

    update future frontend https

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://medium-blog-clone.vercel.app'],
+    'allowed_origins' => ['https://mjay88s-medium-blog-clone.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
