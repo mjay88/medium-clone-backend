09138a26092ac33c1ce2678ae94101dc35684872
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-07-20 22:28:25 -0500

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,37 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://mjay88s-medium-blog-clone.vercel.app'],
+
+    'allowed_origins' => [
+        'https://mjay88s-medium-blog-clone.vercel.app',
+        'https://medium-clone-frontend-46jtj2lvi-mjay88s-projects.vercel.app',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
+
 ];
